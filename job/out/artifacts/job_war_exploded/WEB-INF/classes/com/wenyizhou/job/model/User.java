fdf7c6fc195a926e14b741b199232314053ec4ad
wenyizhou <wenyizhou@qq.com>
2019-04-08 14:21:56 +0800

    提交

---
+++ job/out/artifacts/job_war_exploded/WEB-INF/classes/com/wenyizhou/job/model/User.java
@@ -0,0 +1,70 @@
+package com.wenyizhou.job.model;
+
+public class User {
+    private String userId;
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+    private String userPassword;
+    //角色类型 0:学生 1:教师/家长 2:管理员
+    private int roleType;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(int roleType) {
+        this.roleType = roleType;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
+    }
+}
